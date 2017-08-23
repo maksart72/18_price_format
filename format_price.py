cc2dc9ac73bce8c74ba89a21ad00dcf88f785bf2
maksart72 <maksim.artemyev@gmail.com>
2017-08-23 18:05:36 +0500

    Draft

--- format_price.py
+++ format_price.py
@@ -1,5 +1,17 @@
+import sys
+
+
 def format_price(price):
-    pass
+
+    integer_part, fractional_part = price.split('.')
+    integer_part = '{0:,}'.format(int(integer_part)).replace(',', ' ')
+    fractional_part = str(round(float('0.%s'%(fractional_part)),2))[-2:]
+    pretty_price = integer_part+'.'+fractional_part
+    return pretty_price
 
 if __name__ == '__main__':
-    pass
+    if not len(sys.argv) > 1:
+        print("Error: Empty argument, try format_price.py <price>")
+        exit()
+    price = sys.argv[1]
+    print(format_price(price))
