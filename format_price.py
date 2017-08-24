a3605d9091204fe00a9a8d75ede5887ba4fb0f56
maksart72 <maksim.artemyev@gmail.com>
2017-08-24 14:03:54 +0500

    Draft

--- format_price.py
+++ format_price.py
@@ -3,9 +3,9 @@ import sys
 
 def format_price(price):
 
+    price = str(round(float(price), 2))
     integer_part, fractional_part = price.split('.')
     integer_part = '{0:,}'.format(int(integer_part)).replace(',', ' ')
-    fractional_part = str(round(float('0.%s'%(fractional_part)),2))[-2:]
     pretty_price = integer_part+'.'+fractional_part
     return pretty_price
 
